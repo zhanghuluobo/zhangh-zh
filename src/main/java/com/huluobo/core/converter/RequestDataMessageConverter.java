b69387fe7e915727c08c7017fe53e570a4c39956
zhangh <18516865802@163.com>
2018-06-13 19:39:45 +0800

    修改转换requestData方法

--- src/main/java/com/huluobo/core/converter/RequestDataMessageConverter.java
+++ src/main/java/com/huluobo/core/converter/RequestDataMessageConverter.java
@@ -1,10 +1,17 @@
 package com.huluobo.core.converter;
 
 
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
+import org.springframework.http.converter.AbstractGenericHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.mvc.method.annotation.RequestResponseBodyMethodProcessor;
 
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.List;
 
 /**
@@ -14,9 +21,21 @@ import java.util.List;
  * 版权所有 ZH
  **/
 
-public class RequestDataMessageConverter extends RequestResponseBodyMethodProcessor {
+public class RequestDataMessageConverter extends AbstractGenericHttpMessageConverter {
 
-    public RequestDataMessageConverter(List<HttpMessageConverter<?>> converters, ContentNegotiationManager manager, List<Object> requestResponseBodyAdvice) {
-        super(converters, manager, requestResponseBodyAdvice);
+
+    @Override
+    protected void writeInternal(Object o, Type type, HttpOutputMessage httpOutputMessage) throws IOException, HttpMessageNotWritableException {
+        System.out.println(1);
+    }
+
+    @Override
+    protected Object readInternal(Class aClass, HttpInputMessage httpInputMessage) throws IOException, HttpMessageNotReadableException {
+        return null;
+    }
+
+    @Override
+    public Object read(Type type, Class aClass, HttpInputMessage httpInputMessage) throws IOException, HttpMessageNotReadableException {
+        return null;
     }
 }
