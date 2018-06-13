957bb3e6ccf5f52b4198b5d2f8e54a6ed0b23f00
zhangh <18516865802@163.com>
2018-06-13 23:23:18 +0800

    封装Converter

---
+++ src/main/java/com/huluobo/core/config/DefaultMvcAdapterConfig.java
@@ -0,0 +1,22 @@
+package com.huluobo.core.config;
+
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.huluobo.core.converter.RequestDataMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DefaultMvcAdapterConfig {
+    public DefaultMvcAdapterConfig() {
+    }
+    
+    public static RequestMappingHandlerAdapter requestMappingHandlerAdapter(RequestMappingHandlerAdapter requestMappingHandlerAdapter, FastJsonHttpMessageConverter fastJsonHttpMessageConverter, RequestDataMessageConverter requestDataMessageConverter){
+        List<HttpMessageConverter<?>> converters = new ArrayList<>();
+        converters.add(requestDataMessageConverter);
+        List<HandlerMethodArgumentResolver> argumentResolvers = new ArrayList<>();
+
+    }
+}
--- src/main/java/com/huluobo/core/config/WebMvcConfig.java
+++ src/main/java/com/huluobo/core/config/WebMvcConfig.java
@@ -1,12 +1,18 @@
 package com.huluobo.core.config;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.huluobo.core.converter.RequestDataMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    public RequestMappingHandlerAdapter requestMappingHandlerAdapter(FastJsonHttpMessageConverter fastJsonHttpMessageConverter,RequestDataMessageConverter requestDataMessageConverter){
+        return
+    }
 
     @Bean
     public RequestDataMessageConverter requestDataMessageConverter(){
