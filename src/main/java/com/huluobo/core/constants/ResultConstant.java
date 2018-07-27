a46336bf99f967bbed411dd28b511311cbeb6acf
zhangh <18516865802@163.com>
2018-07-27 09:02:37 +0800

    1、引入insdepUI 2、封装正确提示，错误提示

---
+++ src/main/java/com/huluobo/core/constants/ResultConstant.java
@@ -0,0 +1,22 @@
+package com.huluobo.core.constants;
+
+/**
+ * 返回结果常量
+ *
+ * @author zhanghui
+ * @since 2018/7/25 14:00
+ * 版权所有 ZH
+ **/
+
+public class ResultConstant {
+
+    public static final String INSERT_SUCCESS = "插入成功";
+    public static final String INSERT_FAIL = "插入失败";
+    public static final String UPDATE_SUCCESS = "更新成功";
+    public static final String UPDATA_FAIL ="更新失败";
+    public static final String DELETE_SUCCESS = "删除成功";
+    public static final String DELETE_FAIL = "删除失败";
+    public static final String INSERT_UPDATE_SUCCESS = "插入/更新成功";
+    public static final String INSERT_UPDATE_FAIL = "插入/更新失败";
+
+}
--- src/main/webapp/static/easyui/huluobo.js
+++ src/main/webapp/static/easyui/huluobo.js
@@ -14,5 +14,39 @@
             }
         })
         return serializeObj;
-    }
+    };
+    
+    $.messageShowSuccess = function (msg) {
+        $.messager.show({
+            msg: msg,
+            showType:'show',
+            height:80,
+            slide:'none',
+            style:{
+                left:'',
+                right:0,
+                top:document.body.scrollTop+document.documentElement.scrollTop+60,
+                bottom:'',
+                border:'none',
+                background:'#afcf24'
+            }
+        });
+    };
+
+    $.messageShowFail = function (msg) {
+        $.messager.show({
+            msg: msg,
+            showType:'show',
+            height:80,
+            slide:'none',
+            style:{
+                left:'',
+                right:0,
+                top:document.body.scrollTop+document.documentElement.scrollTop+60,
+                bottom:'',
+                border:'none',
+                background:'#df4916'
+            }
+        });
+    };
 })(jQuery);
